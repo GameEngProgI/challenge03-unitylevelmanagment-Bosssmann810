f6038b6add6511a4380059096e5a5ac8a176a49b
Bosssmann810 <sealered@gmail.com>
2026-01-21 15:14:56 -0400

    transition locked state has been added to make sure it wont rapidly switch levels, (also end of day 2)

--- Assets/levelchangetrigger.cs
+++ Assets/levelchangetrigger.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class levelchangetrigger : MonoBehaviour
 {
-
+    public GameObject player;
+    private bool transitionlockedstate;
     public LevelManager _levelManager;
     public GameObject _levelToActivate;
     public Transform _spawnLocal;
@@ -11,18 +12,29 @@ public class levelchangetrigger : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = SurviceHub.Instance.playerController.gameObject;
         _levelManager = SurviceHub.Instance.levelManager;
+    }
+    private void OnEnable()
+    {
+        transitionlockedstate = GetComponent<Collider2D>().OverlapPoint(player.transform.position);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("entered");
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && transitionlockedstate == false)
         {
             _levelManager.levelchange(_levelToActivate, _spawnLocal);
 
         }
 
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            transitionlockedstate = false;
+        }
     }
 
     // Update is called once per frame
