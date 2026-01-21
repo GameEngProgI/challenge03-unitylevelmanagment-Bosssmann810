5a6b006a8be21b7f232903642708832f36b4096d
Bosssmann810 <sealered@gmail.com>
2026-01-21 14:15:00 -0400

    you can now walk off screen and spawn at a new position, its like a whole new level loaded

--- Assets/Systems/LevelManager.cs
+++ Assets/Systems/LevelManager.cs
@@ -2,23 +2,12 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    public GameObject currentlevel;
-    public GameObject nextlevel;
+    public Transform playerlocation;
 
-    public void levelchange()
+    public void levelchange(GameObject leveltoload, Transform spawn, GameObject leveltodespawn)
     {
-        currentlevel.SetActive(false);
-        nextlevel.SetActive(true);
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+        leveltodespawn.SetActive(false);
+        leveltoload.SetActive(true);
+        playerlocation.position = spawn.position;
     }
 }
--- Assets/levelchangetrigger.cs
+++ Assets/levelchangetrigger.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 public class levelchangetrigger : MonoBehaviour
 {
 
-    public LevelManager levelmanager; 
-
+    public LevelManager _levelManager;
+    public GameObject _levelToActivate;
+    public Transform _spawnLocal;
+    public GameObject _currentLevel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        levelmanager = SurviceHub.Instance.levelManager;
+        _levelManager = SurviceHub.Instance.levelManager;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("entered");
         if (collision.CompareTag("Player"))
         {
-            levelmanager.levelchange();
+            _levelManager.levelchange(_levelToActivate, _spawnLocal, _currentLevel);
 
         }
 
